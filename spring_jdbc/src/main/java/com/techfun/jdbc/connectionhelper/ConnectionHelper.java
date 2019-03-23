74680c71ea444f969c8f6dd5b74528d719ce167a
Acer\Aspire <cherryy552@gmail.com>
2019-03-23 16:18:19 +0630

    connection file created.

---
+++ spring_jdbc/src/main/java/com/techfun/jdbc/connectionhelper/ConnectionHelper.java
@@ -0,0 +1,26 @@
+package com.techfun.jdbc.connectionhelper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper {
+
+	private static final String DB_DRIVER = "org.postgresql.Driver";
+	private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/Exercise";
+	private static final String DB_USER = "postgres";
+	private static final String DB_PASSWORD = "root";
+	
+	public static Connection getDBConnection() throws ClassNotFoundException {
+		Connection dbConnection = null;
+		try {
+			Class.forName(DB_DRIVER);
+			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,DB_PASSWORD);
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return dbConnection;
+	}
+	
+}
